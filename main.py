bf8c9355eed7c6a29366645b7bed4015101a758f
anasco119 <revo9techno@gmail.com>
2025-07-01 14:45:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -279,7 +279,7 @@ from flask import Flask, render_template, session, request, redirect, url_for
 app = Flask(__name__)
 app.secret_key = 'anki_secret'  # سر الجلسة لتخزين البيانات مؤقتًا
 
-cards = generate_anki_cards_from_text(text)
+cards = generate_anki_cards_from_text(content, major="General", user_id=uid, num_cards=15)
 
 @app.route('/anki', methods=['GET', 'POST'])
 def anki_cards():
